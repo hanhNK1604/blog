6ced4d3cdcd819cf99500a3872f67f1539ba2f17
hanhNK1604 <hoanganhnk1604@gmail.com>
2025-08-29 20:38:16 +0700

    fix routing

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "hanhnk1604",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
